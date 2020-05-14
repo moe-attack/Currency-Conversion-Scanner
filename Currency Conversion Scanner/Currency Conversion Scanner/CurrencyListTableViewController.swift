b0ad81aa9ae4685e541b1326e6158fc6f2ad73ba
Jimmy Low <lemoning.low@gmail.com>
2020-05-14 13:44:09 +0800

    [Feat] Displaying real fetched data with reference to local default_currency. [Fix] Fixed issue where persistentcontainer.viewcontext isn't saved.

--- Currency Conversion Scanner/Currency Conversion Scanner/CurrencyListTableViewController.swift
+++ Currency Conversion Scanner/Currency Conversion Scanner/CurrencyListTableViewController.swift
@@ -60,9 +60,10 @@ class CurrencyListTableViewController: UITableViewController, DatabaseListener, 
             let cell = tableView.dequeueReusableCell(withIdentifier: CURRENCY_CELL, for: indexPath) as! CurrencyListCell
             let country = countries[indexPath.row]
             cell.countryLabel.text = country.name?.uppercased()
-            if let rate = country.currency?.sgd{
-                cell.rateLabel.text = String(format: "1 %@ = %@ %@", defaultCurrency, String(roundUpDouble(number: rate)), country.currencyAbbreviation!).uppercased()
-            }
+            
+            let rate = 1.0 / getRateFromCurrency(currency: country.currency, abbre: defaultCurrency)
+            cell.rateLabel.text = String(format: "1 %@ = %@ %@", defaultCurrency, String(roundUpDouble(number: rate)), country.currencyAbbreviation!).uppercased()
+            
             if let name = country.name {
                 cell.flagIcon.image = UIImage(named: name)
             }
@@ -112,7 +113,7 @@ class CurrencyListTableViewController: UITableViewController, DatabaseListener, 
     
     func onCountryChange(change: DatabaseOperation, countries: [Country]) {
         self.countries = countries
-        tableView.reloadData()
+        tableView.reloadSections([CURRENCY_CELL_INDEX], with: .automatic)
     }
     
     func roundUpDouble(number: Double) -> Double{
@@ -126,4 +127,276 @@ class CurrencyListTableViewController: UITableViewController, DatabaseListener, 
             loadCurrency(url: url, country: country)
         }
     }
+    
+    func loadCurrency(url: String, country: Country){
+        URLSession.shared.invalidateAndCancel()
+        // PercentageEncoding must be added to handle non-alphabetical and non-number characters
+        let jsonURL = URL(string: url.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!)
+        let task = URLSession.shared.dataTask(with: jsonURL!) { (data, response, error) in
+            if let error = error {
+                print(error)
+                return
+            }
+            
+            do {
+                let decoder = JSONDecoder()
+                let currencyData = try decoder.decode(RawCurrencyData.self, from: data!)
+                if let rates = currencyData.rates {
+                    self.currency = rates
+                    DispatchQueue.main.async {
+                        let currencyObject = self.databaseController?.createCurrency()
+                        country.currency = currencyObject
+                        if let cad = self.currency?.cad {
+                            currencyObject?.cad = Double(cad)
+                        }
+                        if let hkd = self.currency?.hkd {
+                            currencyObject?.hkd = Double(hkd)
+                        }
+                        if let php = self.currency?.php {
+                            currencyObject?.php = Double(php)
+                        }
+                        if let dkk = self.currency?.dkk {
+                            currencyObject?.dkk = Double(dkk)
+                        }
+                        if let huf = self.currency?.huf {
+                            currencyObject?.huf = Double(huf)
+                        }
+                        if let czk = self.currency?.czk {
+                            currencyObject?.czk = Double(czk)
+                        }
+                        if let gbp = self.currency?.gbp {
+                            currencyObject?.gbp = Double(gbp)
+                        }
+                        if let ron = self.currency?.ron {
+                            currencyObject?.ron = Double(ron)
+                        }
+                        if let sek = self.currency?.sek {
+                            currencyObject?.sek = Double(sek)
+                        }
+                        if let idr = self.currency?.idr {
+                            currencyObject?.idr = Double(idr)
+                        }
+                        if let inr = self.currency?.inr {
+                            currencyObject?.inr = Double(inr)
+                        }
+                        if let brl = self.currency?.brl {
+                            currencyObject?.brl = Double(brl)
+                        }
+                        if let rub = self.currency?.rub {
+                            currencyObject?.rub = Double(rub)
+                        }
+                        if let hrk = self.currency?.hrk {
+                            currencyObject?.hrk = Double(hrk)
+                        }
+                        if let jpy = self.currency?.jpy {
+                            currencyObject?.jpy = Double(jpy)
+                        }
+                        if let thb = self.currency?.thb {
+                            currencyObject?.thb = Double(thb)
+                        }
+                        if let chf = self.currency?.chf {
+                            currencyObject?.chf = Double(chf)
+                        }
+                        if let eur = self.currency?.eur {
+                            currencyObject?.eur = Double(eur)
+                        }
+                        if let myr = self.currency?.myr {
+                            currencyObject?.myr = Double(myr)
+                        }
+                        if let bgn = self.currency?.bgn {
+                            currencyObject?.bgn = Double(bgn)
+                        }
+                        if let try0 = self.currency?.try0 {
+                            currencyObject?.try0 = Double(try0)
+                        }
+                        if let cny = self.currency?.cny {
+                            currencyObject?.cny = Double(cny)
+                        }
+                        if let nok = self.currency?.nok {
+                            currencyObject?.nok = Double(nok)
+                        }
+                        if let nzd = self.currency?.nzd {
+                            currencyObject?.nzd = Double(nzd)
+                        }
+                        if let zar = self.currency?.zar {
+                            currencyObject?.zar = Double(zar)
+                        }
+                        if let usd = self.currency?.usd {
+                            currencyObject?.usd = Double(usd)
+                        }
+                        if let mxn = self.currency?.mxn {
+                            currencyObject?.mxn = Double(mxn)
+                        }
+                        if let sgd = self.currency?.sgd {
+                            currencyObject?.sgd = Double(sgd)
+                        }
+                        if let aud = self.currency?.aud {
+                            currencyObject?.aud = Double(aud)
+                        }
+                        if let ils = self.currency?.ils {
+                            currencyObject?.ils = Double(ils)
+                        }
+                        if let krw = self.currency?.krw {
+                            currencyObject?.krw = Double(krw)
+                        }
+                        if let pln = self.currency?.pln {
+                            currencyObject?.pln = Double(pln)
+                        }
+                        
+                        // note that creation are all done in child context, so we need to save the child context and push the
+                        // changes to the main context, then reset the child context for later use.
+                        self.databaseController?.saveChildContext()
+                        self.databaseController?.resetChildContext()
+                    }
+                }
+            } catch let err {
+                print(err)
+            }
+        }
+        task.resume()
+    }
+    
+    func getRateFromCurrency(currency: Currency?, abbre: String) -> Double {
+        var retRate = 0.0
+        switch abbre{
+        case "CAD":
+            if let rate = currency?.cad {
+                retRate = rate
+            }
+        case "HKD":
+            if let rate = currency?.hkd {
+                retRate = rate
+            }
+        case "ISK":
+            if let rate = currency?.isk {
+                retRate = rate
+            }
+        case "PHP":
+            if let rate = currency?.php {
+                retRate = rate
+            }
+        case "DKK":
+            if let rate = currency?.dkk {
+                retRate = rate
+            }
+        case "HUF":
+            if let rate = currency?.huf {
+                retRate = rate
+            }
+        case "CZK":
+            if let rate = currency?.czk {
+                retRate = rate
+            }
+        case "GBP":
+            if let rate = currency?.gbp {
+                retRate = rate
+            }
+        case "RON":
+            if let rate = currency?.ron {
+                retRate = rate
+            }
+        case "SEK":
+            if let rate = currency?.sek {
+                retRate = rate
+            }
+        case "IDR":
+            if let rate = currency?.idr {
+                retRate = rate
+            }
+        case "INR":
+            if let rate = currency?.inr {
+                retRate = rate
+            }
+        case "BRL":
+            if let rate = currency?.brl {
+                retRate = rate
+            }
+        case "RUB":
+            if let rate = currency?.rub {
+                retRate = rate
+            }
+        case "HRK":
+            if let rate = currency?.hrk {
+                retRate = rate
+            }
+        case "JPY":
+            if let rate = currency?.jpy {
+                retRate = rate
+            }
+        case "THB":
+            if let rate = currency?.thb {
+                retRate = rate
+            }
+        case "CHF":
+            if let rate = currency?.chf {
+                retRate = rate
+            }
+        case "EUR":
+            if let rate = currency?.eur {
+                retRate = rate
+            }
+        case "MYR":
+            if let rate = currency?.myr {
+                retRate = rate
+            }
+        case "BGN":
+            if let rate = currency?.bgn {
+                retRate = rate
+            }
+        case "TRY":
+            if let rate = currency?.try0 {
+                retRate = rate
+            }
+        case "CNY":
+            if let rate = currency?.cny {
+                retRate = rate
+            }
+        case "NOK":
+            if let rate = currency?.nok {
+                retRate = rate
+            }
+        case "NZD":
+            if let rate = currency?.nzd {
+                retRate = rate
+            }
+        case "ZAR":
+            if let rate = currency?.zar {
+                retRate = rate
+            }
+        case "USD":
+            if let rate = currency?.usd {
+                retRate = rate
+            }
+        case "MXN":
+            if let rate = currency?.mxn {
+                retRate = rate
+            }
+        case "SGD":
+            if let rate = currency?.sgd {
+                retRate = rate
+            }
+        case "AUD":
+            if let rate = currency?.aud {
+                retRate = rate
+            }
+        case "ILS":
+            if let rate = currency?.ils {
+                retRate = rate
+            }
+        case "KRW":
+            if let rate = currency?.krw {
+                retRate = rate
+            }
+        case "PLN":
+            if let rate = currency?.pln {
+                retRate = rate
+            }
+        default:
+            ()
+        }
+        
+        return retRate
+    }
+    
+    
 }
