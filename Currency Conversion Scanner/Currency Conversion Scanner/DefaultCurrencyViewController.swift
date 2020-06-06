c24ab789d6952327c2c03b75c7a3e4c3e8dfef23
Jimmy Low <lemoning.low@gmail.com>
2020-06-06 11:40:07 +0800

    [Feat] Added Default Currency change feature

---
+++ Currency Conversion Scanner/Currency Conversion Scanner/DefaultCurrencyViewController.swift
@@ -0,0 +1,50 @@
+//
+//  DefaultCurrencyViewController.swift
+//  Currency Conversion Scanner
+//
+//  Created by Jimmy Low on 22/5/20.
+//  Copyright © 2020 Jimmy Low. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class DefaultCurrencyViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+        
+    @IBOutlet weak var currencyPicker: UIPickerView!
+    @IBAction func saveButton(_ sender: UIButton) {
+        buttonTapped()
+    }
+    
+    let pickerData = Constants.allCurrencies.ALL_CURRENCIES.sorted{ $0.country < $1.country }
+    var selectedItem = ("", "")
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        currencyPicker.delegate = self
+        currencyPicker.dataSource = self
+        selectedItem = pickerData[0]
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerData.count
+    }
+    
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return String(format: "%@ - %@", pickerData[row].country, pickerData[row].abbre)
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        selectedItem = pickerData[row]
+    }
+    
+    func buttonTapped(){
+        navigationController?.popViewController(animated: true)
+        UserDefaults.standard.set(selectedItem.1, forKey: "DefaultCurrency")
+    }
+}
--- Currency Conversion Scanner/Currency Conversion Scanner/MenuTableViewController.swift
+++ Currency Conversion Scanner/Currency Conversion Scanner/MenuTableViewController.swift
@@ -29,58 +29,39 @@ class MenuTableViewController: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return 2
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MenuCell", for: indexPath)
+        cell.textLabel?.font = UIFont.systemFont(ofSize: 17, weight: UIFont.Weight.heavy)
+        switch indexPath.row{
+        case 0:
+            cell.textLabel?.text = "Calculator"
+        case 1:
+            cell.textLabel?.text = "Change Default Currency"
+        default:
+            ()
+        }
         return cell
     }
-    */
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        switch indexPath.row {
+        case 0:
+            ()
+        case 1:
+            performSegue(withIdentifier: "defaultCurrencySegue", sender: nil)
+        default:
+            ()
+        }
+        tableView.deselectRow(at: indexPath, animated: true)
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
 
     /*
     // MARK: - Navigation
